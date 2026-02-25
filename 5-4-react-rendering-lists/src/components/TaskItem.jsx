4d287a8d71411fd430a70cb23dcd34cbecc4c47e
janadamdam <s202323190@kfupm.edu.sa>
2026-02-25 13:15:35 +0300

    TODO3 complete

--- 5-4-react-rendering-lists/src/components/TaskItem.jsx
+++ 5-4-react-rendering-lists/src/components/TaskItem.jsx
@@ -12,10 +12,12 @@ export default function TaskItem({ task, onToggle, onDelete }) {
         {/* TODO (TASK 3): Show <DueBadge /> ONLY when task is NOT done (&&)
             Example:
               {!task.isDone && <DueBadge dueDate={task.dueDate} />} */}
+              {!task.isDone && <DueBadge dueDate={task.dueDate} />}
 
         {/* TODO (TASK 2): Display the task title
             Example:
               <span className="title">{task.title}</span> */}
+              <span className="title">{task.title}</span>
       </label>
 
       {/* TODO (TASK 4): Delete button calls onDelete(task.id) on click */}
